ffaf3fcdcf618e53e435182a0dad5d537e89ce63
brodix78 <brodnikov1950@gmail.com>
2020-10-26 22:05:30 +0400

    2. IndexControl. Таблица и вид.[2092#304134] - исправления

--- src/main/java/ru/job4j/accident/controller/IndexControl.java
+++ src/main/java/ru/job4j/accident/controller/IndexControl.java
@@ -1,5 +1,6 @@
 package ru.job4j.accident.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,16 @@ import ru.job4j.accident.service.AccidentService;
 
 @Controller
 public class IndexControl {
+
+    private AccidentService service;
+
+    public IndexControl(AccidentService service) {
+        this.service = service;
+    }
+
     @GetMapping("/")
     public String index(Model model) {
-        AccidentService service = new AccidentService();
-        model.addAttribute("accidents", service.allAccidents());
+        model.addAttribute("accidents", this.service.allAccidents());
         return "index";
     }
 }
--- src/main/java/ru/job4j/accident/service/AccidentService.java
+++ src/main/java/ru/job4j/accident/service/AccidentService.java
@@ -1,15 +1,19 @@
 package ru.job4j.accident.service;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.repository.AccidentMem;
-
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@Service
 public class AccidentService {
-    AccidentMem mem =  new AccidentMem();
+
+    private AccidentMem mem;
+
+    public AccidentService(AccidentMem mem) {
+        this.mem = mem;
+    }
 
     public List<Accident> allAccidents() {
         return new ArrayList<>(mem.getAccidents().values());
